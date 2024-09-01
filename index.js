b5045212f00f319e356624c1319551514253ac16
ibrahim sohofi <78817932+ibrahimsohofi@users.noreply.github.com>
2024-09-01 21:33:37 +0100

    update

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ generateQR.addEventListener('click',()=>{
           fetch(img.src).then(Response=>Response.blob()).then(blob=>{
             const qrUrl = window.URL.createObjectURL(blob);
             downloadImg.href=qrUrl;
-            downloadImg.setAttribute('download','Qrcode.png');
+            downloadImg.setAttribute('download');
             window.URL.revokeObjectURL(qrUrl);
           })
         })
